a044d09918979b0103e7b22239cd27809ea53949
Ravid-jam <ravid.jam02@gmail.com>
2024-10-14 17:09:59 +0530

    skelatan and loadding functinality added.

--- src/app/homepage/components/Product.tsx
+++ src/app/homepage/components/Product.tsx
@@ -78,6 +78,11 @@ export default function Product() {
         const res = await fetch(
           `https://dummyjson.com/products?limit=${limit}&skip=${skip}`
         );
+
+        if (!res.ok) {
+          throw new Error(`Network response was not ok: ${res.statusText}`);
+        }
+
         const data: IProduct = await res.json();
         setProducts((prev) => [...prev, ...data.products]);
         setTotal(data.total);
@@ -95,11 +100,14 @@ export default function Product() {
   useEffect(() => {
     const observer = new IntersectionObserver(
       (entries) => {
-        if (entries[0].isIntersecting && !loading && products.length < total) {
-          setSkip((prev) => prev + limit); // Increase the skip count to load more data
-        }
+        entries.forEach((entry) => {
+          console.log("Intersection Observer Entry:", entry);
+          if (entry.isIntersecting && !loading && products.length < total) {
+            setSkip((prev) => prev + limit); // Increase the skip count to load more data
+          }
+        });
       },
-      { threshold: 1.0 }
+      { threshold: 0.1 } // Adjust threshold if necessary
     );
 
     if (observerRef.current) {
@@ -125,66 +133,101 @@ export default function Product() {
             </p>
           </div>
         </div>
-
         <div className="grid gap-3 2xl:grid-cols-6 xl:grid-cols-4 md:grid-cols-3 grid-cols-2 sm:px-0 px-2">
-          {products.map((row: Product, index: number) => (
-            <div key={index}>
-              <Card className="!p-0 ">
-                <CardHeader className="!p-0 !h-22">
-                  <ProductCarousel slides={row.images} />
-                </CardHeader>
-                <CardContent className="!py-1 !px-2">
-                  <div>
-                    <h3
-                      className="text-base font-serif truncate cursor-pointer hover:underline text-[#8A8AA2] w-full"
-                      onClick={() => router.push(`/details/${row.id}`)}
-                    >
-                      {row.title}
-                    </h3>
-                    <div className="">
-                      <p className="text-lg font-bold font-mono text-gray-600 line-through">
-                        <span className="text-base">₹</span>
-                        {row.discountPercentage}
-                      </p>
-                      <p className="text-[#353543] font-bold text-2xl font-mono">
-                        <span className="text-lg">₹</span>
-                        {row.price}
-                      </p>
-                    </div>
-                    <div className="flex items-center md:flex-nowrap flex-wrap gap-2 my-3">
-                      <div className="flex items-center border rounded-full p-2 bg-[#23BB75]">
-                        <svg
-                          className="w-4 h-4 text-white me-1"
-                          aria-hidden="true"
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="currentColor"
-                          viewBox="0 0 22 20"
-                        >
-                          <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                        </svg>
-                        <p className="ms-2 font-mono text-sm font-bold text-white">
-                          {row.rating}
-                        </p>
+          {loading ? (
+            <>
+              {Array.from({ length: limit }).map((_, index) => (
+                <Card
+                  className="!p-0 border border-gray-200 animate-pulse"
+                  key={index}
+                >
+                  <CardHeader className="!p-0">
+                    <div className="h-48 bg-gray-300 rounded-t-xl"></div>
+                  </CardHeader>
+                  <CardContent className="!py-1 !px-2">
+                    <div>
+                      <div className="h-4 bg-gray-300 rounded w-3/2 mb-2"></div>
+                      <div className="h-4 bg-gray-300 rounded w-3/4 mb-2"></div>
+                      <div className="h-4 bg-gray-300 rounded w-1/2 mb-2"></div>
+                      <div className="flex items-center md:flex-nowrap flex-wrap gap-2 my-3">
+                        <div className="flex items-center border rounded-full p-2 w-20 bg-gray-300">
+                          <div className="h-4 w-4 bg-white rounded-full"></div>
+                          <p className="ms-2 font-mono text-sm font-bold text-white"></p>
+                        </div>
+                        <div className="h-4 bg-gray-300 rounded w-1/2"></div>
                       </div>
-                      <a
-                        href="#"
-                        className="sm:text-base text-sm font-medium font-sans text-gray-500 hover:underline dark:text-white"
-                      >
-                        {row.reviews.length} Reviews
-                      </a>
                     </div>
-                  </div>
-                </CardContent>
-                <CardFooter className="flex justify-between gap-x-2 !pt-1 !px-2 !pb-3">
-                  <div className="flex-1">
-                    <Button className="w-full !bg-[#004AAC]">
-                      Add To Cart
-                    </Button>
-                  </div>
-                </CardFooter>
-              </Card>
-            </div>
-          ))}
+                  </CardContent>
+                  <CardFooter className="flex justify-between gap-x-2 !pt-1 !px-2 !pb-3">
+                    <div className="flex-1">
+                      <Button className="w-full !bg-gray-300"></Button>
+                    </div>
+                  </CardFooter>
+                </Card>
+              ))}
+            </>
+          ) : (
+            <>
+              {products.map((row: Product, index: number) => (
+                <div key={index}>
+                  <Card className="!p-0 ">
+                    <CardHeader className="!p-0 !h-22">
+                      <ProductCarousel slides={row.images} />
+                    </CardHeader>
+                    <CardContent className="!py-1 !px-2">
+                      <div>
+                        <h3
+                          className="text-base font-serif truncate cursor-pointer hover:underline text-[#8A8AA2] w-full"
+                          onClick={() => router.push(`/details/${row.id}`)}
+                        >
+                          {row.title}
+                        </h3>
+                        <div className="">
+                          <p className="text-lg font-bold font-mono text-gray-600 line-through">
+                            <span className="text-base">₹</span>
+                            {row.discountPercentage}
+                          </p>
+                          <p className="text-[#353543] font-bold text-2xl font-mono">
+                            <span className="text-lg">₹</span>
+                            {row.price}
+                          </p>
+                        </div>
+                        <div className="flex items-center md:flex-nowrap flex-wrap gap-2 my-3">
+                          <div className="flex items-center border rounded-full p-2 bg-[#23BB75]">
+                            <svg
+                              className="w-4 h-4 text-white me-1"
+                              aria-hidden="true"
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="currentColor"
+                              viewBox="0 0 22 20"
+                            >
+                              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                            </svg>
+                            <p className="ms-2 font-mono text-sm font-bold text-white">
+                              {row.rating}
+                            </p>
+                          </div>
+                          <a
+                            href="#"
+                            className="sm:text-base text-sm font-medium font-sans text-gray-500 hover:underline dark:text-white"
+                          >
+                            {row.reviews.length} Reviews
+                          </a>
+                        </div>
+                      </div>
+                    </CardContent>
+                    <CardFooter className="flex justify-between gap-x-2 !pt-1 !px-2 !pb-3">
+                      <div className="flex-1">
+                        <Button className="w-full !bg-[#004AAC]">
+                          Add To Cart
+                        </Button>
+                      </div>
+                    </CardFooter>
+                  </Card>
+                </div>
+              ))}
+            </>
+          )}
         </div>
 
         {/* Observer element for infinite scroll */}
@@ -193,7 +236,9 @@ export default function Product() {
         {/* Loading indicator */}
         {loading && (
           <div className="text-center py-5">
-            <p>Loading more products...</p>
+            <div className="flex items-center justify-center">
+              <div className="h-8 w-8 animate-spin rounded-full border-4 border-[#004AAC] border-t-transparent" />
+            </div>
           </div>
         )}
       </div>
